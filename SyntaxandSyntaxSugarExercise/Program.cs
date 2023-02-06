c65c2c35dfee3209266c9feacf0b8b825571aa40
sawyer tarr <sawyertarr@outlook.com>
2023-02-06 13:14:42 -0600

    InitialCommit

---
+++ SyntaxandSyntaxSugarExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
